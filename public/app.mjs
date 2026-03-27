adb673215910ec6490955973664e76e80c7f6f6c
CYvoll <79579521+CYvoll@users.noreply.github.com>
2026-03-27 11:35:20 +0100

    js til mjs

--- public/app.mjs
+++ public/app.mjs
@@ -9,7 +9,7 @@ import "./components/dashboard.mjs";
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", async () => {
     try {
-      await navigator.serviceWorker.register("/service-worker.js");
+      await navigator.serviceWorker.register("/service-worker.mjs");
       console.log("Service worker registered");
     } catch (error) {
       console.error("Service worker registration failed:", error);
